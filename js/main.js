64ee2979a708a3d6673b90db79a729449272f24d
jprajzendanc <j.prajzendanc@gmail.com>
2022-10-28 11:28:45 +0200

    1-responsywna-sekcja-bootstrap + 1-pobierz-dane-programisty

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$(document).ready(function () {
+    $("button").click(() => {
+        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php").then(
+            (val) => {
+                const dane = $("#dane-programisty");
+                dane.append($(`<p>Imię: ${val.imie}</p>`));
+                dane.append($(`<p>Nazwisko: ${val.nazwisko}</p>`));
+                dane.append($(`<p>Zawód: ${val.zawod}</p>`));
+                dane.append($(`<p>Firma: ${val.firma}</p>`));
+            }
+        )
+    })
+});
 
